8905c4cba53758f02254f847bccf969454161fbc
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-10-30 08:25:24 +0900

    add sound panel

--- src/components/Metronome/Sound.vue
+++ src/components/Metronome/Sound.vue
@@ -1,14 +1,26 @@
 <template>
-    <div>{{msg}}</div>
+    <mtrm-component type="lifting" :title="title" :params="serializeSounds" :val="sound"></mtrm-component>
 </template>
 <style></style>
 <script>
-    /* eslint-disable indent */
+    /* eslint-disable indent,key-spacing,comma-dangle,semi,spaced-comment,padded-blocks,func-call-spacing */
     export default {
         name: 'MetronomeSound',
         data () {
             return {
-                msg: 'Metronome Sound'
+                title : 'SOUND',
+                sound : 1,
+                sounds: [
+                    {id: 1, file: '../../sound/s_01.mp3', active: false},
+                    {id: 2, file: '../../sound/s_02.mp3', active: false},
+                    {id: 3, file: '../../sound/s_03.mp3', active: false},
+                    {id: 4, file: '../../sound/s_04.mp3', active: false},
+                ]
+            }
+        },
+        computed: {
+            serializeSounds: function () {
+                return JSON.stringify (this.sounds);
             }
         }
     }
